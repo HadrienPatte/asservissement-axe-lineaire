860728b2a4826492df7f777c58e828df5aeac05c
Hadrien Patte <hadrien.patte@protonmail.com>
2017-02-19 01:44:15 +0100

    V3 boucle fermee maquette 1

--- maquette_1/boucle_fermee/boucle_fermee.ino
+++ maquette_1/boucle_fermee/boucle_fermee.ino
@@ -7,21 +7,21 @@
   Implementation d'un asservissement proportionel
   Implementation de la vitesse instantanee par integration de la position
 
-  V 2  Hadrien Patte 28/01/2017
+  V 3  Hadrien Patte 19/02/2017
   ----------------------------------------------------------------------------------------------------------------------------*/
 /* -------------------------------------------------------------------------------------------------------------------------
   Notes : * pas de pulldown internes sur des arduino, uniquement des pullup
             volatile pour les variables modifiables en interruption
             const pour les pin
   ----------------------------------------------------------------------------------------------------------------------------*/
-
+long consigne = 6000;
 volatile long position = 0;             // position mesuree par le codeur
 
 long previousMillis = 0;                // pour l'affichage de la position sur le port serie
 
 
 // Connexion du driver moteur L298N aux broches numeriques Arduino
-const int pinPWM          =  12;     // Pin PWM enable
+const int pinPWM              =  12;     // Pin PWM enable
 const int pinMoteur1          =  11;     // Pin Moteur 1
 const int pinMoteur2          =  10;     // Pin Moteur 2
 
@@ -33,18 +33,27 @@ int stateCodeurB             =   0;     // Variable d'etat du codeur B
 
 const int ilsDroitPin  =  14;     // Broche signal de l'ILS droit
 const int ilsGauchePin =  15;     // Broche signal de l'ILS gauche (celui qui sert pour le homing)
-int homingSpeed        = 125;     // Vitesse de homing
 
-int vitesse            = 150;
+int homingSpeed        = 200;     // Vitesse de homing
+int vitesse            = 255;
 
-long consigne          = 5000;
 
 volatile bool sens = false;
 
 float vitesseInstantanee = 0;
-long previousposition = 0;
 
 int period = 50;
+
+//PID
+int periodPID = 100;
+float kp = 1;
+float ki = 0.2;
+float kd = 0.2;
+//float previousError = 0;
+float previousPosition;
+float sumError;
+float output;
+unsigned long previousTime;
 
 void setup() {
   Serial.begin(115200);           // Activation de la communication sur le port série
@@ -66,25 +75,26 @@ void setup() {
 
   homing();
 
-  attachInterrupt(0, doEncoderMotor, CHANGE);   // Execute doEncoderMotor function on interrupt 0 - pin 2
+  attachInterrupt(0, doEncoderMotorA, CHANGE); // interruption 0 sur le codeur A (PIN 2)
+  attachInterrupt(1, doEncoderMotorB, CHANGE); // interruption 1 sur le codeur B (PIN 3)
   // triggered by a "CHANGE". Means RISING ou FALLING edge
 
-  previousposition = position;
+  //previousposition = position;
   //deplacementPosition(consigne);
 }
 
 void loop() {
 
-
+  correcteur();
   // Print encoder position every "period" through the serial port
   if (millis() > previousMillis + period )  {
-    //Serial.println(sens);
-    if (vitesseInstantanee != 0.0) {
+    Serial.println(output/1000);
+    /*if (vitesseInstantanee != 0.0) {
       //Serial.print("vitesseInstantanee = ");
       Serial.println(vitesseInstantanee);
-    }
-    vitesseInstantanee = 1000 * (position - previousposition) / period;
-    previousposition = position;
+      }*/
+    //vitesseInstantanee = 1000 * (position - previousposition) / period;
+    //previousposition = position;
     previousMillis = millis();
   }
 
@@ -104,6 +114,8 @@ void loop() {
       deplacementGauche();
     }
     }*/
+
+
   if (consigne > position + 10) {
     deplacementDroite();
   }
@@ -113,16 +125,7 @@ void loop() {
   else {
     arretMoteur();
   }
-  /*
-    if (consigne - 10 > position) {
-      deplacementDroite();
-    }
-    else if (consigne + 10 < position) {
-      deplacementGauche();
-    }
-    else {
-      arretMoteur();
-    }*/
+
 
 }
 
@@ -182,7 +185,7 @@ void deplacementPosition(long consigne) {
 
 // Code for interrupt 0. Gives the encoder signed cumulated ticks, including CW and CCW (plus and minus) ways
 
-void doEncoderMotor() {
+void doEncoderMotorA() {
   //stateCodeurA = (PINE & B00010000) >> 4;
   //stateCodeurB = (PINH & B00100000) >> 5;   //que pour la pin 8
 
@@ -213,3 +216,53 @@ void doEncoderMotor() {
   }
 }
 // ---------------------------------------------------------------------------------------------------------------------------
+
+void doEncoderMotorB() {
+  //stateCodeurA = (PINE & B00010000) >> 4;
+  //stateCodeurB = (PINH & B00100000) >> 5;   //que pour la pin 8
+
+  stateCodeurA = digitalRead(pinCodeurA);
+  stateCodeurB = digitalRead(pinCodeurB);
+  if (stateCodeurA == HIGH) {              // Found a low-to-high on A phase. if(digitalRead(pinCodeurA)==HIGH){ .... read PE4
+    if (stateCodeurB == LOW) {             // Check B phase to see which way. if(digitalRead(pinCodeurB)==LOW) { .... read PH5
+      position ++ ;           // CCW
+      sens = false;
+    }
+
+    else {
+      position -- ;                      // CW
+      //sens = true;
+    }
+  }
+
+  else {
+    if (stateCodeurB == LOW) {             // Check B phase to see which way. if(digitalRead(pinCodeurB)==LOW) { .... read PH5
+      position -- ;                       // CCW
+      sens = true;
+    }
+
+    else {
+      position ++ ;                       // CW
+      //sens = false;
+    }
+  }
+}
+// ---------------------------------------------------------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------------------------------------------------------
+void correcteur() {
+  // Correcteur PID
+  unsigned long now = millis();
+  if ((now - previousTime) >= periodPID) {
+    // on fait les calculs
+    float error = consigne - position;
+    sumError += error;
+    float dPosition = position - previousPosition;
+    // on calcule output
+    output = kp * error + ki * sumError - kd * dPosition;
+
+    // pour le prochain passage
+    previousPosition = position;
+    previousTime = now;
+  }
+}
